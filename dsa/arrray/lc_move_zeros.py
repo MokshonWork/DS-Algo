3b7ed25ffa47096146fef9ccc56899ef338d6904
MokshonWork <221651574+MokshonWork@users.noreply.github.com>
2026-06-21 09:36:44 +0530

    lc_move_zeros updated

--- dsa/arrray/lc_move_zeros.py
+++ dsa/arrray/lc_move_zeros.py
@@ -3,7 +3,7 @@
 nums = [ 1,0,2,4,3,0,0,3,5,1]
 
 class Solution:
-    def moveZeroes(self, nums: list[int]) :
+    def moveZeroes(self, nums: list[int]) :#-> brute
         n = len(nums)
         temp = []
         for i in range(0,n):
@@ -11,11 +11,36 @@ class Solution:
                 temp.append(nums[i])
 
         n_temo = len(temp)
-        for i in range(o,n_temo):
+        for i in range(0,n_temo):
             nums[i] = temp[i]
 
         for i in range(n_temo,n):
             nums[i] =0
-            
+    
+    def move_Zeroes(self, nums: list[int]) :#-> OPTIMAL
+        if len(nums) ==1:
+            return
+        i = 0
+        while i < len(nums):
+            if nums[i] ==  0:
+                break
+            i+=1
+        if i ==len(nums):
+            return
+        j=i+1
+        while j<len(nums):
+            if nums[j]!=0:
+                nums[i],nums[j]= nums[j],nums[i]
+                i+=1
+            j+=1
+
+sol = Solution()      
+sol.moveZeroes(nums)
+# print(nums)
+
+sol.move_Zeroes(nums)
+print(nums)
+
+
 
 
